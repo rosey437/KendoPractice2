3a76b98df9aa010b6e14214e905ec5128dd007e5
rosey437 <36466906+rosey437@users.noreply.github.com>
2020-03-23 14:32:19 -0600

    Add files via upload
    new files

---
+++ JsonReadWrite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KendoPractice2.Models
+{
+    public class JsonReadWrite
+    {
+        public string Read(string fileName, string location)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(  Directory.GetCurrentDirectory(), root, location,
+            fileName);
+
+            string jsonResult;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+            return jsonResult;
+        }
+
+        public void Write(string fileName, string location, string jSONString)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine( Directory.GetCurrentDirectory(), root, location,
+            fileName);
+
+            using (var streamWriter = File.CreateText(path))
+            {
+                streamWriter.Write(jSONString);
+            }
+        }
+    }
+}
